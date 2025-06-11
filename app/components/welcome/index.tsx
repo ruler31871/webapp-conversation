dbd01c53a1c5310210aae0ae5c8f153a0262c1ff
ruler31871 <727678611@qq.com>
2025-06-11 14:34:44 +0800

    Update index.tsx

--- app/components/welcome/index.tsx
+++ app/components/welcome/index.tsx
@@ -380,10 +380,12 @@ const Welcome: FC<IWelcomeProps> = ({
               </div>
               : <div>
               </div>}
-            <a className='flex items-center pr-3 space-x-3' href="https://dify.ai/" target="_blank">
-              <span className='uppercase'>{t('app.chat.powerBy')}</span>
-              <FootLogo />
-            </a>
+            {/* 删除 Powered by Dify 标识
+<a className='flex items-center pr-3 space-x-3' href="https://dify.ai/" target="_blank">
+  <span className='uppercase'>{t('app.chat.powerBy')}</span>
+  <FootLogo />
+</a>
+*/}
           </div>
         )}
       </div>
